7323ef6dc1cb23c45d7d83a35fb064776ade839d
neelima <neelima.benapally@gmail.com>
2020-04-03 16:13:19 +0100

    added API Documentation

--- node_lab_1/index.js
+++ node_lab_1/index.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import moviesRouter from './api/movies';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 
 dotenv.config();
 
@@ -9,8 +11,11 @@ const app = express();
 
 const port = process.env.PORT;
 
+const swaggerDocument = YAML.load('./../movie-api-yaml/swagger.yaml');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
 app.use(express.static('public'));
