3ba4a9ad2c1b3ac2d43d1760dc678070f67d6d0e
neelima <neelima.benapally@gmail.com>
2020-04-03 16:09:13 +0100

    added routing for add,update,delete movie

--- node_lab_1/index.js
+++ node_lab_1/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import bodyParser from 'body-parser';
 import moviesRouter from './api/movies';
 
 dotenv.config();
@@ -8,8 +9,9 @@ const app = express();
 
 const port = process.env.PORT;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.use(express.static('public'));
-
 app.use('/api/movies', moviesRouter);
 app.use(express.static('public'));
 
